c6b158c3f91ed66b117e105a38608a99a9f4d20c
Vladimir <vladimir.vishnevsky@db.com>
2023-05-06 00:38:57 +0000

    label fix?

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,13 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "3.4.0"
-
-    project_id   = var.project
-    network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-      {
-          subnet_name   = "gaurav-subnet"
-          subnet_ip     = "10.100.10.0/24"
-          subnet_region = var.region
-      }
-    ]
-
+resource "google_compute_network" "vpc_network" {
+  project_id                 = var.project # Replace this with your project ID in quotes
+  network_name                    = "my-auto-mode-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+  ]
 }
