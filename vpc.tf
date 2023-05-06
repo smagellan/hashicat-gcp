64774ef2222f500fa254f4ce1aeea76d9a068e69
Vladimir <vladimir.vishnevsky@db.com>
2023-05-06 00:51:25 +0000

    vpc fix?

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "test-vpc-module-02" {
-  source = "../.."
+  source  = "terraform-google-modules/network/google//modules/subnets"
+  version = "3.4.0"
   project_id   = var.project
   network_name = "gaurav-network"
 
