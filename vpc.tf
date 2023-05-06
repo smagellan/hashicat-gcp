0325595598b397b14651bbcb3f2824da07fe0ef4
Vladimir <vladimir.vishnevsky@db.com>
2023-05-06 00:44:31 +0000

    vpc fix?

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,12 @@
-resource "google_compute_network" "vpc_network" {
-  project_id                 = var.project # Replace this with your project ID in quotes
-  network_name                    = "my-auto-mode-network"
-  auto_create_subnetworks = true
-  mtu                     = 1460
+module "test-vpc-module-02" {
+  project_id   = var.project
+  network_name = "gaurav-network"
+
   subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
-  ]
+ ]
 }
